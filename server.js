bc742d0c85048220be3770cc3c84a8b1ec337314
19WH1A0524 <harshinirayasam03@gmail.com>
2023-02-14 20:34:34 +0530

    pushing html only

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ const port = process.env.PORT || 5010;
 
 app.get("/", function(req, res){
 	// res.send("I am me"); this is me 
-	res.sendFile(__dirname+"/index.html")
+	// res.sendFile(__dirname+"/index.html")
+	res.sendFile(__dirname+"/resume.html")
 });
 
 app.listen(port, function(){
