4cd7e9f723f1224fc74d789d79237a9c2ed24f96
19WH1A0524 <harshinirayasam03@gmail.com>
2023-02-15 22:00:33 +0530

    updated server.js with db connection

--- server.js
+++ server.js
@@ -1,14 +1,29 @@
+require('dotenv').config(); //.env
+const mongoose = require('mongoose')
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5010;
-
+const connectionString = process.env.MONGO_CONNECTION_STRING;
 app.get("/", function(req, res){
-	// res.send("I am me"); this is me 
-	// res.sendFile(__dirname+"/index.html")
+	// res.send("I am me"); 
+	// res.sendFile(__dirname+"/simple.html")
 	res.sendFile(__dirname+"/resume.html")
+	// res.sendFile(__dirname+"/card.html")
 });
-
-app.listen(port, function(){
-	console.log("Server running on http://localhost:"+port);
-	console.log(`Server running on http://localhost:${port}`);
+app.get("/card", function(req, res){
+	
+	res.sendFile(__dirname+"/card.html")
+});
+mongoose.set('strictQuery', true);
+mongoose.connect(connectionString, {}, function(err){ // we are tring to connect if cannot error 
+	if (err){
+		console.error(err);
+	}
+	else{
+		console.log("DB Connected-------------")
+		app.listen(port, function(){
+			// console.log("Server running on http://localhost:"+port);
+			console.log(`Server running on http://localhost:${port}`);
+		});
+	}
 });
