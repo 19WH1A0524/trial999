0823c5a04e9eb5d1d504ee3a1b0ce2bcb7ec5da0
19WH1A0524 <harshinirayasam03@gmail.com>
2023-02-16 22:11:53 +0530

    updated all crud operations of user

--- backend/lib/userLib.js
+++ backend/lib/userLib.js
@@ -1,25 +1,75 @@
 const userModel = require("../models/userModel");
 
-module.exports.getAllUsers = async function(callBack){
-    try{
-        var users = await userModel.find({});
+module.exports.getAllUsers = async function (callBack) {
+    try {
+        var users = await userModel.find({ isDelete: false });
         callBack(null, users) // error is null , gives users
     }
-    catch(err){
+    catch (err) {
         callBack(err, null) //error is displayed, no users
     }
 }
-module.exports.createFirstUser = async function(callBack){
-    try{
+module.exports.createFirstUser = async function (callBack) {
+    try {
         var user = {
             userName: "harshini",
-            yearOfGraduation : 2023
+            yearOfGraduation: 2023
         };
         var newUser = new userModel(user);
         var result = await newUser.save();
         callBack(null, result);
     }
-    catch(err){
+    catch (err) {
         callBack(err, null) //error is displayed, no users
     }
 }
+module.exports.createUser = async function (user, callBack) {
+    try {
+        var newUser = new userModel(user);
+        var result = await newUser.save();
+        callBack(null, result);
+    }
+    catch (err) {
+        callBack(err, null) //error is displayed, no users
+    }
+}
+
+module.exports.updateUser = async function (username, data, callBack) {
+    try {
+        var query = {
+            userName: username,
+        };
+        // var data = {
+        //     yearOfGraduation : data,
+        // };
+        var result = await userModel.updateOne(query, data);
+        callBack(null, result);
+    }
+    catch (err) {
+        callBack(err, null)
+    }
+}
+module.exports.getUserByFilter = async function (filter, callBack) {
+    try {
+        var result = await userModel.findOne(filter);
+        callBack(null, result);
+    }
+    catch (err) {
+        callBack(err, null);
+    }
+}
+
+module.exports.deleteUser = async function (username, callBack) {
+    try {
+        var query = {
+            userName: username,
+        };
+        // var result = await userModel.deleteOne(query, data); // hard delete
+        var result = await userModel.updateOne(query, { isDeleted: true });//soft delete
+        callBack(null, result);
+    }
+    catch (err) {
+        callBack(err, null);
+    }
+}
+
--- server.js
+++ server.js
@@ -5,35 +5,77 @@ const mongoose = require('mongoose');
 const app = express();
 const port = process.env.PORT || 5010;
 // const connectionString = process.env.MONGO_CONNECTION_STRING;
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
 	// res.send("I am me"); 
 	// res.sendFile(__dirname+"/simple.html")
-	res.sendFile(__dirname+"/resume.html")
+	res.sendFile(__dirname + "/resume.html")
 	// res.sendFile(__dirname+"/card.html")
 });
-app.get("/card", function(req, res){
-	
-	res.sendFile(__dirname+"/card.html")
+app.get("/card", function (req, res) {
+
+	res.sendFile(__dirname + "/card.html")
 });
 mongoose.set('strictQuery', true);
-mongoose.connect(process.env.MONGO_CONNECTION_STRING, {}, function(err){ // we are tring to connect if cannot error 
-	if (err){
+mongoose.connect(process.env.MONGO_CONNECTION_STRING, {}, function (err) { // we are tring to connect if cannot error 
+	if (err) {
 		console.error(err);
 	}
-	else{
+	else {
 		console.log('Connected to database');
-        // TODO : donot create a user if atleast 1 user exist in the table
-        userLib.createFirstUser(function(err, res) {
-            if (err) {
-                //console.error(err);
-            } else {
-                console.log(res);
-            }
-        });
-        app.listen(port, function() {
-            // console.log('Server started on port ' + port);
+		// TODO : donot create a user if atleast 1 user exist in the table
+		// userLib.createFirstUser(function(err, res) {
+		//     if (err) {
+		//         //console.error(err);
+		//     } else {
+		//         console.log(res);
+		//     }
+		// });
+		// userLib.createUser({userName: "bz",yearOfGraduation: 2025}, function(err,res){
+		// 	if (err) {
+		// 		        //console.error(err);
+		// 		    } else {
+		// 		        console.log(res);
+		// 		    }
+		// })
+		// userLib.updateUser(function(err,res){
+		// 	if (err){
+		// 		console.error(err);
+		// 	}
+		// 	else{
+		// 		console.log(res)
+		// 	}
+		// })
+		// })
+
+		userLib.deleteUser("harshini", function (err, result) {
+			if (err) {
+				console.error(err);
+			}
+			else {
+				console.log(result);
+			}
+		})
+		// userLib.getUserByFilter({userName : "harshini"}, function(err,res){
+		// 	if(err){
+		// 		console.error(err);
+
+		// 	}
+		// 	else{
+		// 		console.log(res);
+		// 	}
+		// })
+		// userLib.getAllUsers(function(err,res){
+		// 	if (err){
+		// 		console.error(err);
+		// 	}
+		// 	else{
+		// 		console.log(res)
+		// 	}
+		// })
+		app.listen(port, function () {
+			// console.log('Server started on port ' + port);
 			console.log(`Server running on http://localhost:${port}`);
-        });
-		
+		});
+
 	}
 });
